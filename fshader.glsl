19bf5c4e60665a48c09be6a3e576f6094e471b8c
HaipengLi <si_shen_shang@163.com>
2018-01-26 12:10:53 -0800

    init commit, create a window

---
+++ fshader.glsl
@@ -0,0 +1,9 @@
+#version 140
+
+out vec4  fColor;
+
+void
+main()
+{
+    fColor = vec4( 1.0, 0.0, 0.0, 1.0 );
+}
---
+++ vshader.glsl
@@ -0,0 +1,9 @@
+#version 140
+
+in vec4 vPosition;
+
+void
+main()
+{
+    gl_Position = vPosition;
+}
