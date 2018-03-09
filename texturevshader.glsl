4e81d4eeae522999ad55bb9e3380a34d9ce3fc73
HaipengLi <si_shen_shang@163.com>
2018-03-09 13:13:54 -0800

    minor changes of texture shader file

--- texturevshader.glsl
+++ texturevshader.glsl
@@ -1,8 +1,6 @@
 #version 150
 
 in  vec2 vPosition;
-in  vec3 vColor;
-out vec4 color;
 in vec2 vTexPosition;
 out vec2 texPosition;
 
@@ -11,6 +9,5 @@ void main() {
     // The final position of the vertex is assigned to the special `gl_Position` variable
     gl_Position = vec4(vPosition, 0, 1);
     // output any data the fragment shader requires. 
-    color = vec4( vColor, 1.0 );
     texPosition = vTexPosition;
 }
